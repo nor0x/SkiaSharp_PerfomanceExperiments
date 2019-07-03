041e46552debdf7465a0d141372b0d001e8efd98
nor0x <nor0x@users.noreply.github.com>
2019-07-03 11:05:30 +0200

    added repro for SkiaSharp #820

---
+++ Xamarin.Forms/SkiaXFPlayground/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using Xamarin.Forms;
+
+namespace SkiaXFPlayground
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintGLSurfaceEventArgs e)
+        {
+            using (SKPaint p = new SKPaint { IsAntialias = true, StrokeWidth = 2, Typeface = SKTypeface.FromFamilyName("default"), TextSize = 100})
+            {
+                e.Surface.Canvas.DrawText("hello world :-)", 100, 100, p);
+            }
+        }
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
