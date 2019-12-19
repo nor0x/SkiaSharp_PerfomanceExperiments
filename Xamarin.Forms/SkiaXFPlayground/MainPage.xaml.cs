af5b32eb86cddc7803bc03e7e2d57d0e97648ef6
nor0x <nor0x@users.noreply.github.com>
2019-12-19 11:14:42 +0100

    new canvas sample

--- Xamarin.Forms/SkiaXFPlayground/MainPage.xaml.cs
+++ Xamarin.Forms/SkiaXFPlayground/MainPage.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿#define USE_BITMAP
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using SkiaSharp;
@@ -14,22 +18,116 @@ namespace SkiaXFPlayground
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        SKMatrix _currentMatrix;
+
+        /// <summary>
+        /// display scale factor of the current device (retina macbook = 2)
+        /// </summary>
+        double _deviceScaleFactor = 2;
+        double currentScale = 1;
+        double startScale = 1;
+
+        List<MyImage> _images;
+
         public MainPage()
         {
             InitializeComponent();
+            _currentMatrix = SKMatrix.MakeIdentity();
+            _images = new List<MyImage>();
+
+            if (Device.RuntimePlatform == Device.macOS)
+            {
+                var pinch = new PinchGestureRecognizer();
+                pinch.PinchUpdated += Pinch_PinchUpdated;
+                SkiaView.GestureRecognizers.Add(pinch);
+            }
+            SkiaView.EnableTouchEvents = true;
+            SkiaView.Touch += SkiaView_Touch;
+
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly; // you can replace "this.GetType()" with "typeof(MyType)", where MyType is any type in your assembly.
+            byte[] buffer;
+            var names = assembly.GetManifestResourceNames();
+            using (Stream s = assembly.GetManifestResourceStream("SkiaXFPlayground.nasa.jpg"))
+            {
+                if (s != null)
+                {
+                    long length = s.Length;
+                    buffer = new byte[length];
+                    s.Read(buffer, 0, (int)length);
+#if USE_BITMAP
+                    var img = new MyImage() { Location = new SKPoint(300, 300), Image = SKBitmap.Decode(buffer) };
+#else
+                    var img = new MyImage() { Location = new SKPoint(300, 300), Image = SKImage.FromEncodedData(buffer) };
+#endif
+                    _images.Add(img);
+                }
+            }
+        }
+
+        private void SkiaView_Touch(object sender, SkiaSharp.Views.Forms.SKTouchEventArgs e)
+        {
+            switch(e.ActionType)
+            {
+                case SkiaSharp.Views.Forms.SKTouchAction.Moved:
+                    _currentMatrix.TransX = e.Location.X;
+                    _currentMatrix.TransY = e.Location.Y;
+                    SkiaView.InvalidateSurface();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        private void Pinch_PinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+
+            if (e.Status == GestureStatus.Started)
+            {
+                startScale = _currentMatrix.ScaleX;
+            }
+            else if (e.Status == Xamarin.Forms.GestureStatus.Running)
+            {
+                currentScale += (e.Scale - 1) * startScale;
+                currentScale = Math.Max(0.03, currentScale);
+                _currentMatrix.ScaleX = (float)currentScale;
+                _currentMatrix.ScaleY = (float)currentScale;
+                SkiaView.InvalidateSurface();
+            }
+        }
+
+        float GetMagnitude(SKPoint p)
+        {
+            return (float)Math.Sqrt(Math.Pow(p.X, 2) + Math.Pow(p.Y, 2));
         }
 
         void Handle_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintGLSurfaceEventArgs e)
         {
-            using (SKPaint p = new SKPaint { IsAntialias = true, StrokeWidth = 2, Typeface = SKTypeface.FromFamilyName("default"), TextSize = 100})
+            e.Surface.Canvas.SetMatrix(_currentMatrix);
+            e.Surface.Canvas.Clear(SKColors.SeaGreen);
+            using (SKPaint p = new SKPaint { IsAntialias = true, StrokeWidth = 2, Typeface = SKTypeface.FromFamilyName("default"), TextSize = 100, FilterQuality = SKFilterQuality.None})
             {
                 e.Surface.Canvas.DrawText("hello world :-)", 100, 100, p);
+                foreach(var img in _images)
+                {
+#if USE_BITMAP
+                    e.Surface.Canvas.DrawBitmap(img.Image, img.Location, p);
+#else
+                    e.Surface.Canvas.DrawImage(img.Image, img.Location, p);
+#endif
+                }
             }
         }
+    }
 
-        void Handle_Tapped(object sender, System.EventArgs e)
-        {
-
-        }
+    internal class MyImage
+    {
+#if USE_BITMAP
+        public SKBitmap Image { get; set; }
+#else
+        public SKImage Image { get; set; }
+#endif
+        public SKPoint Location { get; set; }
+        public SKSize Size { get; set; }
     }
 }
